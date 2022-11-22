b3f02f3a9d42ad894c0a8fb621e574ef38b8badd
Yuri Moreira <yuriclmoreira@terra.com.br>
2022-11-22 15:59:41 -0300

    implementando e estilizando o carroussel de filmes

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Slick from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+import mockData, { Movie } from "../../data/mock";
+
+
+
+const Poster = ({ cover, title, score }: Movie, index: number) => (
+    <article key={index}>
+        <img src={cover} alt={title} />
+    </article>
+);
+
+interface CarouselData {
+    title?: string;
+    data?: Movie[];
+}
+
+const Carousel = ({ title = 'Filmes em destaque', data = mockData }: CarouselData) => {
+    enum Direction {
+        left,
+        right,
+    }
+
+    const SlickArrow = ({ direction, onClick }: { direction: Direction, onClick?: () => void }) => (
+        <button
+            type='button'
+            className={`absolute w-16 h-full z-10 bg-black bg-opacity-80 top-0 ${direction ? 'right-0' : 'left-0'}`}
+            onClick={onClick}
+        >
+            <FontAwesomeIcon icon={direction ? faChevronRight : faChevronLeft} size='2x' />
+        </button>
+    )
+    const options = {
+        infinite: true,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow: <SlickArrow direction={Direction.left} />,
+        nextArrow: <SlickArrow direction={Direction.right} />,
+    };
+    return (
+        <section>
+            <h2 className="relative z-10 font-bold text-xl ml-8 my-8">{title}</h2>
+            <Slick className="relative mb-8" {...options}>
+                {data.map((movie, index) => Poster(movie, index))}
+            </Slick>
+        </section>
+    );
+
+};
+export default Carousel;
