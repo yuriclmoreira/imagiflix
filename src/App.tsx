997fd7af0c48763e869be2b5fedff1aaf5d78167
Yuri Moreira <yuriclmoreira@terra.com.br>
2023-01-11 17:08:32 -0300

    feat(hero): featched movie from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { URL, APISTRING } from './data/constant'
 import Hero from './components/Hero/Hero';
 import NavBar from './components/Navbar/Navbar';
 import Carousel from './components/Carousel/Carousel';
@@ -7,11 +8,31 @@ import Carousel from './components/Carousel/Carousel';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 const App = () => {
+
+
+
+  const [movies, setMovies] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`);
+
+      const data = await response.json();
+
+      setMovies(data.results);
+
+    }
+
+
+    fetchData();
+  }, []);
+
+  // useEffect(() => movies && console.log(movies), [movies])
+
   return (
     <div className=" m-auto antialiased font-sans text-white bg-black">
-      <Hero />
+      {movies && <Hero {...movies[0]} />}
       <NavBar />
       <Carousel />
       <Carousel />
---
+++ src/components/Score/Score.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './Score.css'
+const Score = ({ value = 5 }: { value?: string | number }) => {
+    const getBorderColor = () => {
+        if (value < 4) return 'border-red-400';
+        if (value < 7) return 'border-yellow-400'
+
+        return 'border-green-400';
+
+    }
+
+    return (
+        <span className={`score inline-block mx-2 py-2 px-3 border-4 bg-black bg-opacity-75 rounded-full ${getBorderColor()}`}>
+            {value}
+        </span>
+    );
+};
+
+export default Score;
